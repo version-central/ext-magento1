05c6eee441f9694c75a556b0a639790a800fdefd
Uwe Kleinmann <uwe@kleinmann.org>
2016-02-29 20:13:10 +0100

    Use combined API token.

--- src/app/code/community/K10r/Versioncentral/Helper/Data.php
+++ src/app/code/community/K10r/Versioncentral/Helper/Data.php
@@ -4,8 +4,7 @@ class K10r_Versioncentral_Helper_Data extends Mage_Core_Helper_Abstract
 {
     const XML_ACTIVE = "k10r_versioncentral/general/active";
     const XML_NAME = "k10r_versioncentral/general/name";
-    const XML_API_IDENTIFIER = "k10r_versioncentral/general/api_identifier";
-    const XML_API_TOKEN = "k10r_versioncentral/general/api_token";
+    const XML_TOKEN = "k10r_versioncentral/general/token";
     const XML_STORE_NAME = "general/store_information/name";
 
     public function getActive()
@@ -18,22 +17,13 @@ class K10r_Versioncentral_Helper_Data extends Mage_Core_Helper_Abstract
         return Mage::getStoreConfig(self::XML_NAME) ? Mage::getStoreConfig(self::XML_NAME) : Mage::getStoreConfig(self::XML_STORE_NAME);
     }
 
-    public function getApiIdentifier()
+    public function getToken()
     {
-        return Mage::getStoreConfig(self::XML_API_IDENTIFIER);
-    }
-
-    public function getApiToken()
-    {
-        return Mage::getStoreConfig(self::XML_API_TOKEN);
+        return Mage::getStoreConfig(self::XML_TOKEN);
     }
 
     public function getAuthorization()
     {
-        return sprintf(
-            '%s:%s',
-            $this->getApiIdentifier(),
-            $this->getApiToken()
-        );
+        return $this->getToken();
     }
 }
