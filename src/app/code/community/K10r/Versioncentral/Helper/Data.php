bf1f44141c0070ed1af58d2a6c9088facd1cb6e9
Uwe Kleinmann <uwe@kleinmann.org>
2016-03-02 17:19:39 +0100

    Remove configurable application name.

--- src/app/code/community/K10r/Versioncentral/Helper/Data.php
+++ src/app/code/community/K10r/Versioncentral/Helper/Data.php
@@ -3,7 +3,6 @@
 class K10r_Versioncentral_Helper_Data extends Mage_Core_Helper_Abstract
 {
     const XML_ACTIVE = "k10r_versioncentral/general/active";
-    const XML_NAME = "k10r_versioncentral/general/name";
     const XML_TOKEN = "k10r_versioncentral/general/token";
     const XML_STORE_NAME = "general/store_information/name";
 
@@ -14,7 +13,7 @@ class K10r_Versioncentral_Helper_Data extends Mage_Core_Helper_Abstract
 
     public function getName()
     {
-        return Mage::getStoreConfig(self::XML_NAME) ? Mage::getStoreConfig(self::XML_NAME) : Mage::getStoreConfig(self::XML_STORE_NAME);
+        return Mage::getStoreConfig(self::XML_STORE_NAME);
     }
 
     public function getToken()
